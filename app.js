b4b0c89882e60b8e7b1b83bd47dd1ea7d38fc274
Jonathan C MacMillan <jmacmill@gmail.com>
2023-05-23 22:25:03 +1000

    Update dependencies

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
 const chalk = require('chalk');
-
+const yargs = require('yargs');
 const getNotes = require('./notes')
 
-console.log(getNotes())
+//Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.bold.green.bgRed('Success'));
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: "Note title", 
+            demandOption: true, 
+            type: 'string'
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log("Title: " + argv.title)
+        console.log("Body: " + argv.body)
+    },
+})
+
+// Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Remove the note')
+    }
+})
+
+// Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('Read a note')
+    }
+})
+
+// List command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: () => {
+        console.log('List all notes')
+    }
+})
+
+
+yargs.parse();
